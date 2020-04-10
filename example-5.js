fa2f235c2b3c1ba0e1d36822b188026ad2da33ef
Camilo Cetina <c.cetina@bookandlearn.com>
2020-04-10 10:31:02 -0500

    CAMILO CETINA EJ5

--- example-5.js
+++ example-5.js
@@ -1,8 +1,10 @@
 import {cleanConsole, createAll} from './data';
+import {camiloEX4} from './example-4';
+
 const companies = createAll();
 
 cleanConsole(5, companies);
-console.log('---- EXAMPLE 5 --- ', 'Put here your function');
+console.log('---- EXAMPLE 5 --- ', camiloEX5());
 
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
@@ -15,24 +17,27 @@ console.log('---- EXAMPLE 5 --- ', 'Put here your function');
 //     'hasCar' => total de "users" propietarios de un carro
 //     'averageWithCar' => edad promedio de los "users" con un carro
 
-// -----------------------------------------------------------------------------
-// INSTRUCTIONS IN ENGLISH
+function camiloEX5() {
+  const Ex4= camiloEX4();
+  const size= Ex4.length;
+  const withCar= Ex4.filter((user) => user.car===true);
+  const hasCar= withCar.length;
+  const average= Math.floor(age(Ex4));
+  const averageWithCar= Math.floor(age(withCar));
+  const report= {
+    'size': size,
+    'usersCar': hasCar,
+    'average': average,
+    'averageWidthCar': averageWithCar,
+  };
+  return report;
+}
 
-// Use the function created in example 4 to create a
-// new function taking as parameter the "companies" variable and returning
-// a new object with the following attributes:
-//     'size' => number of "users"
-//     'average' => average age of "users"
-//     'hasCar' => number of "users" owning a car
-//     'averageWithCar' => average age of users with a car
 
-// -----------------------------------------------------------------------------
-// INSTRUCTIONS EN FRANÇAIS
-
-// Utiliser la fonction créée dans l'exemple 4 pour créer une
-// nouvelle fonction prenant en paramètre la variable "companies" et renvoyant
-// un nouvel objet avec les attributs suivants :
-//     'size' => nombre de "users"
-//     'average' => moyenne d'âge des "users"
-//     'hasCar' => nombre de "users" possédant une voiture
-//     'averageWithCar' => moyenne d'âge des "users" possédant une voiture
+function age(list) {
+  console.log(list);
+  const reducer = ( accumulator, currentValue) => accumulator + currentValue.age;
+  const age = list.reduce(reducer, 0)/list.length;
+  console.log(age);
+  return age;
+}
